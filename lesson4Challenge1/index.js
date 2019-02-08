7cf31dd49175f535cea020116417bfed45661e05
setupshop <gedion.sami@gmail.com>
2019-02-08 00:17:31 -0800

    adding lesson 4 challenges

---
+++ lesson4Challenge1/index.js
@@ -0,0 +1,11 @@
+
+function trigger() {
+  let number = prompt("Enter a number:");
+  alert("Your number squared is: " + number * number);
+}
+
+function trigger2() {
+  let num1 = prompt("Enter first number:");
+  let num2 = prompt("Enter second number:");
+  alert("The product of the two numbers is: " + num1 * num2);
+}
